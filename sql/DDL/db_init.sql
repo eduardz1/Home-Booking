b6c2fd32a550522edba01802ed185ea04e3816cf
Marco Molica <marco.molica@nigithorconsulting.com>
2022-06-15 20:49:17 +0200

    Terminati crud di dati

--- sql/DDL/db_init.sql
+++ sql/DDL/db_init.sql
@@ -8,7 +8,9 @@ DROP TABLE IF EXISTS Utente,
                      Recensione,
                      Commento,
                      Lista,
-                     Contenuto;
+                     Contenuto,
+                     Foto;
+
 DROP TYPE IF EXISTS StatoPrenotazione,
                     CategoriaRecensione,
                     TipologiaAlloggio;
@@ -25,6 +27,7 @@ CREATE TYPE CategoriaRecensione AS ENUM ('Host', 'Utente', 'Alloggio');
 
 -- @block initialize Utente
 -- Superhost: Non si può essere superhost se non si è host
+-- Verificato: Non si può essere verificati se non si ha la carta di identità
 CREATE TABLE Utente (
   Nome VARCHAR(45) NOT NULL,
   Cognome VARCHAR(45) NOT NULL,
@@ -33,8 +36,12 @@ CREATE TABLE Utente (
   DataRegistrazione TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   Host BOOLEAN DEFAULT FALSE NOT NULL,
   Superhost BOOLEAN DEFAULT FALSE NOT NULL,
+  Verificato BOOLEAN DEFAULT FALSE NOT NULL,
+  CartaIdentita VARCHAR(30),	
   PRIMARY KEY (Email),
-  CHECK ((Superhost = TRUE AND Host = TRUE) OR Superhost = FALSE)
+  Constraint user_valid_host CHECK ((Superhost = TRUE AND Host = TRUE) OR Superhost = FALSE),
+  Constraint user_valid_verified CHECK ((Verificato = TRUE AND CartaIdentita IS NOT NULL) OR Verificato = FALSE),
+  Constraint user_valid_email CHECK (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
 );
 
 -- @block initialize Telefono
@@ -59,7 +66,7 @@ CREATE TABLE Pagamento (
 -- Nome: Si potrebbe creare un indice su Nome per velocizzare le operazioni di ricerca sull'alloggio
 -- Host: Può essere NULL per supportare le casistiche in cui un host è stato cancellato dal sistema
 CREATE TABLE Alloggio (
-  IDAlloggio SERIAL,
+  IDAlloggio UUID NOT NULL DEFAULT gen_random_uuid(),
   Nome VARCHAR(45) NOT NULL, 
   Host VARCHAR(45),
   Descrizione TEXT,
@@ -78,27 +85,30 @@ CREATE TABLE Alloggio (
 
 -- @block initialize Prenotazione 
 -- IDAlloggio: Può essere NULL per supportare le casistiche in cui un alloggio è stato cancellato dal sistema
+-- Il check su DataInizio > CURRENT_TIMESTAMP potrebbe non permettere inserimenti massivi futuri da parte di un DBA,
+--  in caso di manutenzione: meglio evitare questo tipo di controllo
 CREATE TABLE Prenotazione (
-  IDPrenotazione SERIAL,
-  IDAlloggio INT,
+  IDPrenotazione UUID NOT NULL DEFAULT gen_random_uuid(),
+  IDAlloggio UUID,
   Richiedente VARCHAR(45) NOT NULL,
   DataInizio TIMESTAMP NOT NULL,
   DataFine TIMESTAMP NOT NULL,
   Soggiorno BOOLEAN DEFAULT FALSE NOT NULL,
   NumeroOspiti INT NOT NULL CHECK (NumeroOspiti >= 0),
   Stato StatoPrenotazione NOT NULL DEFAULT 'Prenotato',
+  Visibile BOOLEAN DEFAULT FALSE NOT NULL,
   FOREIGN KEY (IDAlloggio) REFERENCES Alloggio(IDAlloggio) ON DELETE SET NULL,
   FOREIGN KEY (Richiedente) REFERENCES Utente(Email) ON DELETE CASCADE ON UPDATE CASCADE,
   PRIMARY KEY (IDPrenotazione),
-  CHECK (DataInizio < DataFine),
-  CHECK (DataInizio > CURRENT_TIMESTAMP)
+  Constraint prenotazione_start_before_end CHECK (DataInizio < DataFine)
+  -- CHECK (DataInizio > CURRENT_TIMESTAMP)
 );
 
 -- @block initialize Recensione 
 -- IDPrenotazione: Una prenotazione non può essere cancellata dal sistema, a meno di attività manuali sul db
 CREATE TABLE Recensione (
-    IDRecensione SERIAL,
-    IDPrenotazione INT NOT NULL,
+    IDRecensione UUID NOT NULL DEFAULT gen_random_uuid(),
+    IDPrenotazione UUID NOT NULL,
     Autore VARCHAR(45) NOT NULL,
     DataRecensione DATE NOT NULL,
     ValutazionePosizione INT CHECK(ValutazionePosizione >= 0 AND ValutazionePosizione <= 5),
@@ -110,7 +120,7 @@ CREATE TABLE Recensione (
     FOREIGN KEY (IDPrenotazione) REFERENCES Prenotazione(IDPrenotazione) ON DELETE CASCADE,
     FOREIGN KEY (Autore) REFERENCES Utente(Email) ON DELETE SET NULL ON UPDATE CASCADE,
     PRIMARY KEY (IDRecensione),
-    CHECK (
+    Constraint recensione_valid_category CHECK (
       (Categoria = 'Host' AND ValutazionePosizione IS NULL AND ValutazionePulizia IS NULL AND ValutazioneQualitaPrezzo IS NUll) OR 
       (Categoria = 'Utente' AND ValutazionePosizione IS NULL AND ValutazionePulizia IS NULL AND ValutazioneQualitaPrezzo IS NUll AND ValutazioneComunicazione IS NULL) OR
       (Categoria = 'Alloggio' AND ValutazioneComunicazione IS NULL))
@@ -118,8 +128,8 @@ CREATE TABLE Recensione (
 
 -- @block initialize Commento 
 CREATE TABLE Commento (
-    IDCommento SERIAL,
-    IDRecensione INT NOT NULL,
+    IDCommento UUID NOT NULL DEFAULT gen_random_uuid(),
+    IDRecensione UUID NOT NULL,
     DataCommento TIMESTAMP NOT NULL,
     Autore VARCHAR(45) NOT NULL,
     Corpo TEXT,
@@ -130,7 +140,7 @@ CREATE TABLE Commento (
 
 -- @block initialize Lista
 CREATE TABLE Lista (
-    IDLista SERIAL,
+    IDLista UUID NOT NULL DEFAULT gen_random_uuid(),
     Nome VARCHAR(45) NOT NULL,
     Descrizione TEXT,
     Autore VARCHAR(45) NOT NULL,
@@ -140,9 +150,17 @@ CREATE TABLE Lista (
 
 -- @block initialize Contenuto
 CREATE TABLE Contenuto (
-    IDLista INT NOT NULL,
-    IDAlloggio INT NOT NULL,
+    IDLista UUID NOT NULL,
+    IDAlloggio UUID NOT NULL,
     FOREIGN KEY (IDLista) REFERENCES Lista(IDLista) ON DELETE CASCADE,
     FOREIGN KEY (IDAlloggio) REFERENCES Alloggio(IDAlloggio) ON DELETE CASCADE,
     PRIMARY KEY (IDLista, IDAlloggio)
+);
+
+-- @block initialize Foto
+CREATE TABLE Foto (
+    Path TEXT,
+    IDAlloggio UUID NOT NULL,
+    FOREIGN KEY (IDAlloggio) REFERENCES Alloggio(IDAlloggio) ON DELETE CASCADE,
+    PRIMARY KEY (Path, IDAlloggio)
 );
