e0cd264d6ff77cce72d06ce5136e889bc1c08fc8
eduard <occhipinti.eduard@icloud.com>
2022-07-08 23:33:21 +0200

    relazione v2

--- sql/DDL/3.sql
+++ sql/DDL/3.sql
@@ -2,7 +2,7 @@
 CREATE TABLE Pagamento (
     Utente VARCHAR(45) NOT NULL,
     Numero VARCHAR(16) NOT NULL CHECK (char_length(Numero) = 16),
-    Prefisso VARCHAR(30) NOT NULL,
+    Circuito VARCHAR(30) NOT NULL,
     FOREIGN KEY (Utente) REFERENCES Utente(Email) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (Utente, Numero)
 );
